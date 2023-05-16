167b1578720055116b9bb3507adcee69d2d57c09
Study <lofihiphop360@gmail.com>
2023-05-16 23:42:18 +0300

    Добавление и наполнение контентом домашней страницы. Прописал комментарии

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,1 +1,25 @@
-Hello world!
+<?php
+use app\services\Page;
+?>
+
+<!doctype html>
+<html lang="en">
+<?php
+Page::component('head');
+?>
+<body>
+<?php
+Page::component('navbar');
+?>
+<div class="card text-center">
+    <div class="card-header">
+        Hello
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="/login" class="btn btn-primary">Sign In</a>
+    </div>
+</div>
+</body>
+</html>
